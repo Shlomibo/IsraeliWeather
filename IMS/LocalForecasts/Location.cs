ec7f6584210f30f0e034d52e33af179c05acfccb
Shlomi Borovitz <shlomi.newmail@gmail.com>
2014-09-17 11:33:11 +0300

    Created

---
+++ IMS/LocalForecasts/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.LocalForecasts
+{
+	public sealed class Location
+	{
+		#region Fields
+
+		private readonly string name;
+		private readonly GeoCoordinate
+		#endregion
+	}
+}
